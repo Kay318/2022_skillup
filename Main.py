ed4631dd6e20473369515aab2c1df554cdcb6f78
내비평가스크립트운영실/정용국 <9350816@ict-companion.com>
2022-06-27 17:28:39 +0900

    다국어 자동화1

---
+++ Main.py
@@ -0,0 +1,22 @@
+from UI.MainWindow import Ui_MainWindow
+from UI.Setup_Language import UI_Setup_Language
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox,\
+     QPushButton, QVBoxLayout, QGraphicsScene, QCheckBox, QLabel, QWidget
+import sys
+
+class Main(QMainWindow, Ui_MainWindow, UI_Setup_Language):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setupUi(self)
+        self.Form = QWidget()
+
+        self.set_slot()
+
+    def set_slot(self):
+        self.actionLanguage.triggered.connect(lambda:self.setupUi_Language(self.Form))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv) 
+    myWindow = Main() 
+    myWindow.show() 
+    app.exec_()
